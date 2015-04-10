93e31e708371f9cb28f2543cfcc8a8f068877d85
Roger Bush <rogerbush8@Rogers-MacBook-Pro.local>
2015-04-09 21:48:27 -0700

    Initial impl of Params, Resource, Action

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # grokible-utils-js
+
 Low-level Javascript Library
+
+Grokible utils are a low-level js utility library.  I imagine many js
+authors start with some base set of their own utilities - well this is
+my start of those.
+
+The intent is to have simple, modular, no dependency js that can
+be used both on the front end and on the backend in node modules
+(the unit tests show how to mix the requirejs and require statements).
+The initial use-case is making REST calls, which is about as fundamental
+as one can get.
+
+On the one hand I am developing these as an exercise to perfect my js
+style, but on the other hand, I fully expect this to be a nice, usable,
+fully tested, practical library when I'm done with it.
+
+It's quite possible I'm writing something that has already been written.
+The journey is the reward.
+
+You're certainly welcome to use it, although be advised it's a WIP.
+
+Cheers - Roger
+
+
+
+
+
---
+++ test/Params-test.js
@@ -0,0 +1,88 @@
+
+var requirejs = require ('requirejs');
+var expect = require ('chai').expect;
+
+requirejs.config ({
+    nodeRequire: require,
+    baseUrl : process.cwd (),
+    paths : {
+        Params : 'lib/grokible-utils/Params'
+    }
+});
+
+var Params = requirejs ('Params');
+
+describe ("Params class", function () {
+
+    describe ("constructor (config)", function () {
+        it ("should set internal config and leave targetObject undefined", function () {
+            var pm = new Params ({ hello : "world" });
+            var config = pm.getConfig ();
+            expect (config).to.have.a.property ('hello', 'world');
+            expect (Object.keys (config)).to.have.length (1);
+            expect (pm.getTargetObject ()).to.equal (undefined);
+        });
+    });
+
+    describe ("constructor (config, obj)", function () {
+        it ("should set internal config and targetObject", function () {
+            var obj = {};
+            var pm = new Params ({ hello : "world" }, obj);
+            var config = pm.getConfig ();
+            expect (config).to.have.a.property ('hello', 'world');
+            expect (Object.keys (config)).to.have.length (1);
+            expect (pm.getTargetObject ()).to.equal (obj);
+        });
+    });
+
+    describe ("copy (key)", function () {
+        it ("case 1:  should set object property to same value in config, prefixed by '_'", function () {
+            debugger;
+            var obj = {};
+            var pm = new Params ({ hello : "world", what : "else" }, obj);
+            var x = pm.copy ('hello');
+            expect (obj).to.have.a.property ('_hello', 'world');
+            expect (Object.keys (obj)).to.have.length (1);
+            expect (x).to.equal ('world');
+        });
+    });
+
+    describe ("copy (key)", function () {
+        it ("case 2:  should not set object property if it isn't in config, and return undefined", function () {
+            debugger;
+            var obj = {};
+            var pm = new Params ({ hello : "world", what : "else" }, obj);
+            var x = pm.copy ('missing');
+            expect (Object.keys (obj)).to.have.length (0);
+            expect (x).to.equal (undefined);
+        });
+    });
+
+
+    describe ("copy (key, optDefault)", function () {
+        it ("case 1:  should set object property to same value in config, prefixed by '_'", function () {
+            debugger;
+            var obj = {};
+            var pm = new Params ({ hello : "world", what : "else" }, obj);
+            var x = pm.copy ('hello', 'why not');
+            expect (obj).to.have.a.property ('_hello', 'world');
+            expect (Object.keys (obj)).to.have.length (1);
+            expect (x).to.equal ('world');
+        });
+    });
+
+
+    describe ("copy (key, optDefault)", function () {
+        it ("case 2:  should set object property to default if key/value not present", function () {
+            debugger;
+            var obj = {};
+            var pm = new Params ({ hello : "world", what : "else" }, obj);
+            var x = pm.copy ('missing', 'why not');
+            expect (obj).to.have.a.property ('_missing', 'why not');
+            expect (Object.keys (obj)).to.have.length (1);
+            expect (x).to.equal ('why not');
+        });
+    });
+
+});
+

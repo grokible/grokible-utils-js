1a7bcea8f3da32d28d9dda756edea28c1e563e27
Roger Bush <rogerbush8@Rogers-MacBook-Pro.local>
2015-07-14 16:26:32 -0700

    JqxGridWrapper changes

--- test/JqxGridWrapper.js
+++ test/JqxGridWrapper.js
@@ -7,10 +7,17 @@ var ArgTypeException = require ('ArgTypeException');
 
 describe ("JqxGridWrapper object", function () {
     describe ("constructor", function () {
-        it ("should Check for column array", function () {
-            var jquery = undefined;
+        var jquery = undefined;
+        it ("should throw on wrong option columnDefs type", function () {
             TestExpect.throws (function () {
                 new JqxGridWrapper (jquery, { columnDefs : 1 });
+            }, ArgTypeException, /columnDefs config option is not a Map/);
+        });
+
+        // TODO - this test is wrong, it fails...
+        it ("should throw on columnDef that omits name", function () {
+            TestExpect.throws (function () {
+                new JqxGridWrapper (jquery, { columnDefs : { width : 1 } });
             }, ArgTypeException, /columnDefs config option is not a Map/);
         });
     });

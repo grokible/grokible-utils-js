0196f947e29f227ecc6876e30ee24381674825f2
Roger Bush <roger@grokible.com>
2015-07-17 15:10:42 +0000

    added libs to index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 'use strict';
 
 module.exports.Check = require ('Check');
+module.exports.ExceptionUtils = require ('./grokible.exceptionUtils');
 module.exports.ArgTypeException = require ('ArgTypeException');
 module.exports.ArgException = require ('ArgException');
 
-module.exports.QueryParamException = require ('./grokible.queryParamException');
+module.exports.QueryParamException =
+    require ('./grokible.queryParamException');
 module.exports.ParamSpec = require ('./grokible.paramSpec');
 module.exports.QueryParamSpec = require ('./grokible.queryParamSpec');
+module.exports.HttpException = require ('./grokible.httpException');
 
